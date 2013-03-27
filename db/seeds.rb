deddddaaf2ef4d9474a0ab7dfb33064050f1ef5d
nvynn <nazar.vynnse@gmail.com>
2013-03-27 14:50:45 +0200

    Add scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create(	title: 'CoffeeScript',
+			   	description: %{<p>CoffeeScript is JavaScript done right. It provides all of 
+			   				JavaScript's functionality wrapped wrapped in a cleaner, more succinct syntax.</p>},
+				image_url: 'cs.jpg',
+				price: 36.49)
+
+Product.create(	title: 'Programming Ruby 1.9',
+			   	description: %{<p>Ruby is the fastest growing and most exciting dynamic language
+							out there. If you need to get working programs delivered fast,
+							you should add Ruby to your toolbox.</p>},
+				image_url: 'ruby.jpg',
+				price: 99.95)
+
+Product.create(	title: 'Rails Test Prescriptions',
+			   	description: %{<p>Rails Test Prescriptions is a comprehencive guide to testing rails code.</p>},
+				image_url: 'rtp.jpg',
+				price: 47.29)
