68a65211f027fcd97b4bb6fd607602b1a09c7bdf
nvynn <nvynn@softserveinc.com>
2013-04-17 11:19:32 +0300

    Add yield examples

--- vendor/ruby_exaples/yield.rb
+++ vendor/ruby_exaples/yield.rb
@@ -1,1 +1,23 @@
 #yield examples
+
+def example(a, b)
+	p a
+	# some code there
+
+	yield
+
+	# some code there
+	p b
+end
+
+
+
+example('start', 'end') { p 'this is yield' }
+
+def circle(n, a, b)
+	n.times do |i|
+		yield i, i + a * b
+	end
+end
+
+circle(5, 2, 3) { |n, m| p "#{n}: #{m}" }
